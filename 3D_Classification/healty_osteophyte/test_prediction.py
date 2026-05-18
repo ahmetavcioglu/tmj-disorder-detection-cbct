27408f9005a926e6601553c1165bf48f1aacb996
ahmetavcioglu <ahmetavci.ogu@gmail.com>
2026-05-18 21:02:56 +0300

    Initial Commit

---
+++ 3D_Classification/healty_osteophyte/test_prediction.py
@@ -0,0 +1,133 @@
+import os
+import zipfile
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import nibabel as nib
+
+from scipy import ndimage
+
+
+def read_nifti_file(filepath):
+    """Read and load volume"""
+    # Read file
+    scan = nib.load(filepath)
+    # Get raw data
+    scan = scan.get_fdata()
+    return scan
+
+
+def normalize(volume):
+    """Normalize the volume"""
+    min = 400
+    max = 1500
+    volume[volume < min] = min
+    volume[volume > max] = max
+    volume = (volume - min) / (max - min)
+    volume = volume.astype("float32")
+    return volume
+
+
+def resize_volume(img):
+    """Resize across z-axis"""
+    # Set the desired depth
+    desired_depth = 64
+    desired_width = 128
+    desired_height = 128
+    # Get current depth
+    current_depth = img.shape[-1]
+    current_width = img.shape[0]
+    current_height = img.shape[1]
+    # Compute depth factor
+    depth = current_depth / desired_depth
+    width = current_width / desired_width
+    height = current_height / desired_height
+    depth_factor = 1 / depth
+    width_factor = 1 / width
+    height_factor = 1 / height
+    # Rotate
+    img = ndimage.rotate(img, 90, reshape=False)
+    # Resize across z-axis
+    img = ndimage.zoom(img, (width_factor, height_factor, depth_factor), order=1)
+    return img
+
+
+def process_scan(path):
+    """Read and resize volume"""
+    # Read scan
+    volume = read_nifti_file(path)
+    # Normalize
+    volume = normalize(volume)
+    # Resize width, height and depth
+    volume = resize_volume(volume)
+    return volume
+
+
+def validation_preprocessing(volume, label):
+    """Process validation data by only adding a channel."""
+    volume = tf.expand_dims(volume, axis=3)
+    return volume, label
+
+
+def get_model(width=128, height=128, depth=64):
+    """Build a 3D convolutional neural network model."""
+    inputs = keras.Input((width, height, depth, 1))
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.GlobalAveragePooling3D()(x)
+    x = layers.Dense(units=512, activation="relu")(x)
+    x = layers.Dropout(0.3)(x)
+    outputs = layers.Dense(units=1, activation="sigmoid")(x)
+    # Define the model.
+    model = keras.Model(inputs, outputs, name="3dcnn")
+    return model
+
+
+normal_scan_paths = [
+    os.path.join(os.getcwd(), "healty_test", x)
+    for x in os.listdir("healty_test")
+]
+abnormal_scan_paths = [
+    os.path.join(os.getcwd(), "osteophyte_test", x)
+    for x in os.listdir("osteophyte_test")
+]
+
+abnormal_scans = np.array([process_scan(path) for path in abnormal_scan_paths])
+normal_scans = np.array([process_scan(path) for path in normal_scan_paths])
+
+abnormal_labels = np.array([1 for _ in range(len(abnormal_scans))])
+normal_labels = np.array([0 for _ in range(len(normal_scans))])
+
+# x_test = np.concatenate((abnormal_scans, normal_scans), axis=0)
+# y_test = np.concatenate((abnormal_labels, normal_labels), axis=0)
+x_test = abnormal_scans
+y_test = abnormal_labels
+
+model = get_model(width=128, height=128, depth=64)
+model.summary()
+
+model.load_weights("3d_image_classification.h5")
+
+for i in range(len(x_test)):
+    prediction = model.predict(np.expand_dims(x_test[i], axis=0))[0]
+    scores = [1 - prediction[0], prediction[0]]
+
+    class_names = ["normal", "abnormal"]
+    for score, name in zip(scores, class_names):
+        print(
+            "This model is %.2f percent confident that CT scan is %s"
+            % ((100 * score), name)
+        )
